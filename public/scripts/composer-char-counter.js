4fdb9cdbc82f36faff1dfd46407c86ad7359e4ab
Carmen <sdluojm@gmail.com>
2019-09-11 09:54:19 +0000

    implemented character counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,4 +3,44 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(function() {
+    $('#textArea').on('keypress', function() {
+        let maxlength = parseInt($(this).siblings('span').text());
+        console.log(maxlength);
+        let val = $(this).val();
+        let vallength = val.length;
+        $(this).siblings('span').html(maxlength - vallength);
+        if (vallength > maxlength) {
+            $(this).siblings('span').css('color', 'red');
+        }
+    })
+})
+alert(maxlength - vallength);
 
+// $('#textArea').on('keyup', function() {
+
+//     // Store the maxlength and value of the field.
+//     var maxlength = $(this).attr('maxlength');
+//     var val = $(this).val();
+
+//     var vallength = val.length;
+//     $("#counter-div").html(vallength);
+
+//     // red color
+//     if (vallength >= 10) {
+//         $('#counter-div').css('color', 'red');
+//     }
+//     if (vallength <= 9) {
+//         $('#counter-div').css('color', '');
+//     }
+
+//     // Trim the field if it has content over the maxlength.
+//     if (vallength > maxlength) {
+//         $(this).val(val.slice(0, maxlength));
+//     }
+// });
+
+// //
+// $('textArea').on('blur', function() {
+//     $('#counter-div').hide('slow');
+// });
